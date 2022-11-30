1fe9e86eaa2f176d7610aa5b80122f64c12469b0
Kamil Orzechowski <kam.kr.orzechowski@gmail.com>
2022-11-30 10:02:39 +0100

    The logic of FGroupElement constructor has been changed so that an FGroupElement object stores the minimal array of vertices (including only those where the function graph changes its slope).

--- group.mjs
+++ group.mjs
@@ -1,15 +1,22 @@
 export class FGroupElement {
-    constructor(vertices) {
-        this.vertices = vertices;
-        this.domainSubdivision = vertices.map(v => v[0]);
-        this.imageSubdivision = vertices.map(v => v[1]);
+    constructor(points) {
+        const slopes = getSlopes(points);
+        const predicate = (item, index) =>
+            index == 0 || index == points.length - 1
+            || slopes[index - 1] != slopes[index];
+
+        this.vertices = points.filter(predicate);
+        this.slopes = slopes.filter(predicate);
+        this.domainSubdivision = this.vertices.map(v => v[0]);
+        this.imageSubdivision = this.vertices.map(v => v[1]);
     }
-    getSlopes() {
-        let slopes = [];
-        for (let i = 0; i <= this.vertices.length - 2; i++) {
-            slopes.push((this.imageSubdivision[i + 1] - this.imageSubdivision[i])
-                / (this.domainSubdivision[i + 1] - this.domainSubdivision[i]));
-        }
-        return slopes;
+}
+
+function getSlopes(points) {
+    let slopes = [];
+    for (let i = 0; i <= points.length - 2; i++) {
+        slopes.push((points[i + 1][1] - points[i][1])
+            / (points[i + 1][0] - points[i][0]));
     }
+    return slopes;
 }
--- main.mjs
+++ main.mjs
@@ -1,10 +1,12 @@
 import { BinaryTree, parseToTree } from "./trees.mjs";
 import { FGroupElement } from "./group.mjs";
 
-const tree1 = parseToTree('(a(bc))d');
-const tree2 = parseToTree('((ab)c)d');
-const subdiv1 = parseToTree('(a(bc))d').getIntervalSubdivision();
-const subdiv2 = parseToTree('((ab)c)d').getIntervalSubdivision();
+// Some test cases
+
+const tree1 = parseToTree('a(b(c(df)))');
+const tree2 = parseToTree('a(b((cd)f))');
+const subdiv1 = tree1.getIntervalSubdivision();
+const subdiv2 = tree2.getIntervalSubdivision();
 const zip = (arr1, arr2) => arr1.map((v, i) => [v, arr2[i]]);
 
 const element = new FGroupElement(zip(subdiv1, subdiv2));
@@ -12,4 +14,4 @@ const element = new FGroupElement(zip(subdiv1, subdiv2));
 console.log((element.vertices));
 console.log((element.domainSubdivision));
 console.log((element.imageSubdivision));
-console.log((element.getSlopes()));
+console.log((element.slopes));
