1bc3276b3a30970772ebc0dbd571133112f467f8
Kamil Orzechowski <kam.kr.orzechowski@gmail.com>
2022-12-27 13:07:49 +0100

    Added the function getElementFromWord, which accepts a string of powers of standard generators and computes the corresponding F group element.

--- group.mjs
+++ group.mjs
@@ -1,46 +1,103 @@
 export class FGroupElement {
-    constructor(points) {
-        const slopes = getSlopes(points);
-        const predicate = (item, index) =>
-            index == 0 || index == points.length - 1
-            || slopes[index - 1] != slopes[index];
+  constructor(points) {
+    const slopes = getSlopes(points);
+    const predicate = (item, index) =>
+      index == 0 ||
+      index == points.length - 1 ||
+      slopes[index - 1] != slopes[index];
 
-        this.vertices = points.filter(predicate);
-        this.slopes = slopes.filter(predicate);
-        this.domainSubdivision = this.vertices.map(v => v[0]);
-        this.imageSubdivision = this.vertices.map(v => v[1]);
+    this.vertices = points.filter(predicate);
+    this.slopes = slopes.filter(predicate);
+    this.domainSubdivision = this.vertices.map((v) => v[0]);
+    this.imageSubdivision = this.vertices.map((v) => v[1]);
+  }
+  apply(number) {
+    let i = 0;
+    while (this.domainSubdivision[i + 1] < number) {
+      i++;
     }
-    apply(number) {
-        let i = 0;
-        while (this.domainSubdivision[i + 1] < number) { i++; }
-        return this.imageSubdivision[i] + (number - this.domainSubdivision[i]) * this.slopes[i];
-    }
+    return (
+      this.imageSubdivision[i] +
+      (number - this.domainSubdivision[i]) * this.slopes[i]
+    );
+  }
 }
 
 function getSlopes(points) {
-    let slopes = [];
-    for (let i = 0; i <= points.length - 2; i++) {
-        slopes.push((points[i + 1][1] - points[i][1])
-            / (points[i + 1][0] - points[i][0]));
-    }
-    return slopes;
+  let slopes = [];
+  for (let i = 0; i <= points.length - 2; i++) {
+    slopes.push(
+      (points[i + 1][1] - points[i][1]) / (points[i + 1][0] - points[i][0])
+    );
+  }
+  return slopes;
 }
 
 export const zip = (arr1, arr2) => arr1.map((v, i) => [v, arr2[i]]);
 
 export function inverse(groupElement) {
-    return new FGroupElement(zip(groupElement.imageSubdivision, groupElement.domainSubdivision));
+  return new FGroupElement(
+    zip(groupElement.imageSubdivision, groupElement.domainSubdivision)
+  );
 }
 
 export function compose(element1, element2) {
-    const element2Inverse = inverse(element2);
-    const preimages = element1.domainSubdivision.map(x => element2Inverse.apply(x));
-    const newDomSubdiv = [...new Set(element2.domainSubdivision.concat(preimages)
-        .sort((a, b) => a - b))];
-    return new FGroupElement(newDomSubdiv.map(x => [x, element1.apply(element2.apply(x))]));
+  const element2Inverse = inverse(element2);
+  const preimages = element1.domainSubdivision.map((x) =>
+    element2Inverse.apply(x)
+  );
+  const newDomSubdiv = [
+    ...new Set(
+      element2.domainSubdivision.concat(preimages).sort((a, b) => a - b)
+    ),
+  ];
+  return new FGroupElement(
+    newDomSubdiv.map((x) => [x, element1.apply(element2.apply(x))])
+  );
 }
 
 export const generators = {
-    'A': new FGroupElement([[0.0, 0.0], [0.25, 0.5], [0.5, 0.75], [1.0, 1.0]]),
-    'B': new FGroupElement([[0.0, 0.0], [0.5, 0.5], [0.625, 0.75], [0.75, 0.875], [1.0, 1.0]])
+  A: new FGroupElement([
+    [0.0, 0.0],
+    [0.25, 0.5],
+    [0.5, 0.75],
+    [1.0, 1.0],
+  ]),
+  B: new FGroupElement([
+    [0.0, 0.0],
+    [0.5, 0.5],
+    [0.625, 0.75],
+    [0.75, 0.875],
+    [1.0, 1.0],
+  ]),
 };
+generators["A^{-1}"] = inverse(generators.A);
+generators["B^{-1}"] = inverse(generators.B);
+
+export const Identity = new FGroupElement([
+  [0.0, 0.0],
+  [1.0, 1.0],
+]);
+
+function power(element, exponent) {
+  if (exponent == 0) {
+    return Identity;
+  }
+  let arr;
+  if (exponent > 0) {
+    arr = Array(exponent).fill(element);
+  } else {
+    arr = Array(-exponent).fill(inverse(element));
+  }
+  return arr.reduce(compose);
+}
+
+export function getElementFromWord(string) {
+  const re = /(?<letter>[AB])(?:\^\{(?<exponent>-?[1-9]\d*)\})?/g;
+  const matches = [...string.matchAll(re)];
+  const word = matches.map((x) => [
+    generators[x.groups.letter],
+    x.groups.exponent ? parseInt(x.groups.exponent) : 1,
+  ]);
+  return word.map((arr) => power(arr[0], arr[1])).reduce(compose, Identity);
+}
--- main.mjs
+++ main.mjs
@@ -1,14 +1,21 @@
 import { BinaryTree, parseToTree } from "./trees.mjs";
-import { FGroupElement, zip, inverse, compose, generators } from "./group.mjs";
+import {
+  FGroupElement,
+  zip,
+  inverse,
+  compose,
+  generators,
+  getElementFromWord,
+} from "./group.mjs";
 
 // Some test cases
 
-const tree1 = parseToTree('a(b(c(df)))');
-const tree2 = parseToTree('a(b((cd)f))');
-const subdiv1 = tree1.getIntervalSubdivision();
-const subdiv2 = tree2.getIntervalSubdivision();
+// const tree1 = parseToTree('a(b(c(df)))');
+// const tree2 = parseToTree('a(b((cd)f))');
+// const subdiv1 = tree1.getIntervalSubdivision();
+// const subdiv2 = tree2.getIntervalSubdivision();
 
-const element = new FGroupElement(zip(subdiv1, subdiv2));
+// const element = new FGroupElement(zip(subdiv1, subdiv2));
 // console.log((element.vertices));
 // console.log((element.domainSubdivision));
 // console.log((element.imageSubdivision));
@@ -16,11 +23,19 @@ const element = new FGroupElement(zip(subdiv1, subdiv2));
 
 // console.log('function value of 0.8 = ' + element.apply(0.8) + '\n');
 
-const inv = inverse(element);
-// console.log(inv.vertices);
-// console.log(inv.slopes);
+// const inv = inverse(element);
+// // console.log(inv.vertices);
+// // console.log(inv.slopes);
 
-const A = generators['A'];
-const B = generators['B'];
-const AB = compose(A, B);
-console.log(AB.vertices);
+// const A = generators['A'];
+// const B = generators['B'];
+// const AB = compose(A, B);
+// console.log(AB.vertices);
+
+const element = getElementFromWord("A^{3}B^{-2}");
+console.log(element.vertices);
+console.log(element.apply(5 / 16));
+
+const A = generators.A;
+const B_ = generators["B^{-1}"];
+console.log(A.apply(A.apply(A.apply(B_.apply(B_.apply(5 / 16))))));
