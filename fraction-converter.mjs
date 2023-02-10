c06f55caf1db5c0fd62f7fae4a976d3aa20739f1
Kamil Orzechowski <kam.kr.orzechowski@gmail.com>
2023-02-10 13:35:14 +0100

    Common/decimal switch added, new file fraction-converter.mjs

---
+++ fraction-converter.mjs
@@ -0,0 +1,19 @@
+function parseDyadic(x) {
+  if (Number.isInteger(x)) {
+    return [x, 0];
+  }
+  let numerator = x;
+  let denominatorExp = 0;
+  while (!Number.isInteger(numerator)) {
+    numerator *= 2;
+    denominatorExp += 1;
+  }
+  return [numerator, denominatorExp];
+}
+
+function writeDyadic(x) {
+  const data = parseDyadic(x);
+  return data[1] == 0 ? `${data[0]}` : `${data[0]}/${2 ** data[1]}`;
+}
+
+export { writeDyadic };
--- main.mjs
+++ main.mjs
@@ -1,3 +1,4 @@
+import { writeDyadic } from "./fraction-converter.mjs";
 import { getVertices } from "./group.mjs";
 
 const inputWord = document.getElementById("word");
@@ -17,43 +18,57 @@ validateBtn.addEventListener("click", () => {
   } else {
     validityMsg.textContent = "Invalid input!";
     wordInTeX.textContent = "";
-    plotBtn.setAttribute("disabled", "true")
+    plotBtn.setAttribute("disabled", "true");
   }
 });
 
 const plotBtn = document.getElementById("plot");
 const chart = document.getElementById("chart");
+const commonFrBtn = document.getElementById("commonFr");
+const decimalBtn = document.getElementById("decimalFr");
+
+let option = "common";
+for (const button of [commonFrBtn, decimalBtn]) {
+  button.addEventListener("change", (event) => {
+    option = event.target.value;
+  });
+}
 
 plotBtn.addEventListener("click", () => {
-  plotElement(word);
+  plotElement(word, option);
 });
 
-function plotElement(string) {
-  var vertices = getVertices(string);
-  var data = [{
+function plotElement(string, opt) {
+  let vertices = getVertices(string);
+  let data = {
     x: vertices.map((v) => v[0]),
     y: vertices.map((v) => v[1]),
     type: "scatter",
     mode: "lines+markers",
-    hoverinfo: "x+y"
-  }];
-
-  var layout = {
-    title: 'Graph of g',
-    xaxis: {
-      title: 'argument',
-      hoverformat: '.12~e'
-    },
-    yaxis: {
-      title: 'value',
-      hoverformat: '.12~e'
-    },
-    hovermode: "closest"
   };
 
-  Plotly.newPlot(
-  chart, data, layout
-);
+  if (opt == "common") {
+    data.text = vertices.map(
+      (v) => `(${writeDyadic(v[0])}, ${writeDyadic(v[1])})`
+    );
+    data.hovertemplate = "%{text}<extra></extra>";
+  }
+  if ((opt = "decimal")) {
+    data.hoverinfo = "x+y";
+  }
+
+  const layout = {
+    title: "Graph of g",
+    xaxis: {
+      title: "argument",
+      hoverformat: ".12~e",
+    },
+    yaxis: {
+      title: "value",
+      hoverformat: ".12~e",
+    },
+    hovermode: "closest",
+  };
+
+  Plotly.newPlot(chart, [data], layout);
 }
-
-
